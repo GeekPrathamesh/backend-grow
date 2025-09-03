8c3f880f6713816ad1c88a5ad4c0655482751de7
Prathamesh <prathameshteli727@gmail.com>
2025-09-04 01:22:04 +0530

    .

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,8 @@ import kPassport from "./middleware/passport";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
-app.use(
-  cors({ origin: [process.env.FRONTEND_URL as string], credentials: true })
-);
+app.use(cors({ origin: "*", credentials: true }));
+
 app.use(express.json());
 app.use(cookieParser());
 
